a47a936f0b97cbe80406d22180881226f24ea22c
paul-mcn <paul.a.mcnamara@outlook.com>
2023-05-13 19:20:00 +1000

    changed ui layout

--- views/settings_panel.py
+++ views/settings_panel.py
@@ -18,21 +18,21 @@ class SettingsPanel(tk.Frame):
 
         self.create_inputs()
 
-    def slider_control(self, label_text, command, min, max):
+    def slider_control(self, frame, label_text, command, min, max):
         """Create a slider with a label"""
         # Create a container to hold the label and slider
-        frame = tk.Frame(self)
-        frame.pack(pady=5, padx=10, fill="x")
+        slider_frame = tk.Frame(frame)
+        slider_frame.pack(pady=5, padx=10)
 
         # create label
-        label = tk.Label(frame, text=label_text)
-        label.pack(side="left", padx=(10, 20))
+        label = tk.Label(slider_frame, text=label_text)
+        label.pack(padx=(10, 20), side="left")
 
         # create horizontal slider
         slider = tk.Scale(
-            frame, from_=min, to=max, orient=tk.HORIZONTAL, command=command
+            slider_frame, from_=min, to=max, orient=tk.HORIZONTAL, command=command
         )
-        slider.pack(side="left", fill="x")
+        slider.pack(fill="x")
         return slider
 
     def procedural_array_to_image(self):
@@ -45,49 +45,60 @@ class SettingsPanel(tk.Frame):
         return ImageTk.PhotoImage(img)
 
     def noise_frame(self, image_array):
-        # create a PIL Image object from the NumPy array
-        # image_array = np.random.randint(low=255, size=(100, 100), dtype=np.uint8)
-
         # MUST BE SAVED UNDER "SELF" OTHERWISE PYTHON WILL DELETE IN GARBAGE COLLECTION
         # DONT WORRY, THIS IS WORKING AS INTENDED https://bugs.python.org/issue632323
         self.photo_image = image_array
 
         # create a label widget with the PhotoImage
         image_label = tk.Label(self, image=self.photo_image, width=300, height=300)
-        image_label.pack(side="right")
+        image_label.pack(padx=20, pady=20)
         return image_label
 
     def on_value_change(self):
         self.photo_image = self.procedural_array_to_image()
-        self.image_label.configure(image=self.photo_image, width=300, height=300) # pylint: disable=all
-
+        self.image_label.configure(image=self.photo_image, width=300, height=300)
 
     def create_inputs(self):
         """Create the main window"""
 
+        slider_frame = tk.Frame(self)
+        slider_frame.pack(fill="y", side="left")
         # add buttons and sliders to UI
         width_val = self.slider_control(
-            "width", command=self.controller.set_width, min=0, max=100
+            slider_frame, "width", command=self.controller.set_width, min=0, max=100
         )
         height_val = self.slider_control(
-            "height", command=self.controller.set_height, min=0, max=100
+            slider_frame, "height", command=self.controller.set_height, min=0, max=100
         )
         resolution_val = self.slider_control(
-            "resolution", command=self.controller.set_resolution, min=0, max=200
+            slider_frame,
+            "resolution",
+            command=self.controller.set_resolution,
+            min=0,
+            max=200,
         )
         scale_val = self.slider_control(
-            "scale", command=self.controller.set_scale, min=0, max=100
+            slider_frame, "scale", command=self.controller.set_scale, min=1, max=100
         )
         octaves_val = self.slider_control(
-            "octaves", command=self.controller.set_octaves, min=0, max=100
+            slider_frame, "octaves", command=self.controller.set_octaves, min=1, max=25
         )
         persistence_val = self.slider_control(
-            "persistence", command=self.controller.set_persistence, min=0, max=1
+            slider_frame,
+            "persistence",
+            command=self.controller.set_persistence,
+            min=0,
+            max=1,
         )
         max_angle = self.slider_control(
-            "Max angle", command=self.controller.set_max_angle, min=0, max=90
+            slider_frame,
+            "Max angle",
+            command=self.controller.set_max_angle,
+            min=0,
+            max=90,
         )
         foliage_val = self.slider_control(
+            slider_frame,
             "Foliage density",
             command=self.controller.set_foliage_density,
             min=0,
@@ -107,8 +118,7 @@ class SettingsPanel(tk.Frame):
         image_array = self.procedural_array_to_image()
         self.image_label = self.noise_frame(image_array)
 
-        
-        # update noise image when values are changed
+        # update procedurally generated image when values change
         self.controller.set_on_value_change(self.on_value_change)
 
         button_frame = tk.Frame(self)
